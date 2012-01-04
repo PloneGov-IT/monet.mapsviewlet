8efb4189f714345ae8829968a1fff8d813161330
mircoangelini <mirco.angelini@redturtle.it>
2012-01-04 10:56:56 +0000

    added classifiers

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setup(name='monet.mapsviewlet',
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Plone :: 3.3",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Development Status :: 4 - Beta",
